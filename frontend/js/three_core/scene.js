729e2b76a302594414785989ec0d73eb16f3b2bf
Rulo21 <rulomds21@gmail.com>
2026-04-22 13:39:37 -0600

    cambios en los .ong

--- frontend/js/three_core/scene.js
+++ frontend/js/three_core/scene.js
@@ -86,7 +86,7 @@ export function iniciarVisor(mapa) {
     const geometria = new THREE.SphereGeometry(500, 60, 40);
     geometria.scale(-1, 1, 1);
     const cargadorTextura = new THREE.TextureLoader();
-    const mapaIcono = cargadorTextura.load('/assets/src/arrow1.png',
+    const mapaIcono = cargadorTextura.load('/assets/src/arrow1.webp',
         () => console.log("✅ Textura de flecha cargada correctamente"),
         undefined,
         (err) => console.error("❌ Error cargando la flecha:", err)
